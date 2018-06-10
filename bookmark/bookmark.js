aeacee8b7af934f5d25dce9fe9b7f4553ce311c3
a-kodama <40099513+ecitonon@users.noreply.github.com>
2018-06-10 19:37:37 +0900

    add new file

---
+++ bookmark/bookmark.js
@@ -0,0 +1,43 @@
+// granbluefantasy
+// function granbluefantasy_game() {
+// 	window.open("http://game.granbluefantasy.jp/#top", "_blank", "width=518, height=1014, toolbar=no, scrollbars=yes");
+// }
+// function granbluefantasy_motocal() {
+// 	window.open("http://hsimyu.net/motocal/", "_blank", "fullscreen=yes, scrollbars=yes");
+// }
+function granbluefantasy_raidfinder() {
+	window.open("https://granblue-raidfinder.herokuapp.com/", "_blank", "width=768, height=1014, toolbar=no, scrollbars=yes");
+}
+
+// SNS
+function mstdn_jp() {
+	window.open("https://mstdn.jp/", "_blank", "fullscreen=yes, scrollbars=yes");
+}
+function mstdn_workers() {
+	window.open("https://mstdn-workers.com/", "_blank", "fullscreen=yes, scrollbars=yes");
+}
+function plug_dj() {
+	window.open("https://plug.dj/%E3%82%B9%E3%83%8A%E3%83%83%E3%82%AF%E3%81%B8%E3%82%8B%E3%81%A4", "_blank", "width=1000, height=1014, toolbar=no, scrollbars=yes");
+}
+
+// Movie
+// function movie_amazonprimevideo() {
+// 	window.open("https://www.amazon.co.jp/Prime-Video/b/ref=nav__aiv_piv?ie=UTF8&node=3535604051", "_blank", "width=1000, height=1014, toolbar=no, scrollbars=yes");
+// }
+// function movie_hulu() {
+// 	window.open("http://www.hulu.jp/", "_blank", "width=1000, height=1014, toolbar=no, scrollbars=yes");
+// }
+// function movie_netflix() {
+// 	window.open("https://www.netflix.com/jp/", "_blank", "width=1000, height=1014, toolbar=no, scrollbars=yes");
+// }
+// function movie_bch() {
+// 	window.open("http://www.b-ch.com/", "_blank", "width=1000, height=1014, toolbar=no, scrollbars=yes");
+// }
+// function movie_youtube() {
+// 	window.open("https://www.youtube.com/", "_blank", "width=1000, height=1014, toolbar=yes, scrollbars=yes");
+// }
+
+// Others
+function others_24hoursofhappy() {
+	window.open("http://24hoursofhappy.com/", "_blank", "width=1000, height=1014, toolbar=no, scrollbars=yes");
+}
